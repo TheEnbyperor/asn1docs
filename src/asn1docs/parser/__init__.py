1d2fa157cecd2494f2ef0fea877114ac0fdaedb8
Q Misell <q@magicalcodewit.ch>
2026-06-01 18:40:12 +0200

    fix imports

--- src/asn1docs/parser/__init__.py
+++ src/asn1docs/parser/__init__.py
@@ -1,4 +1,11 @@
 from . import lexical_items
+from . import values_types
+from . import built_in_types
+from . import constructed_types
+from . import object_identifier
+from . import constraints
+from . import parametisation
+from . import javadoc
 from . import module
 
 ASN1_GRAMMAR = module.ModuleDefinition.ignore(lexical_items.comment)
